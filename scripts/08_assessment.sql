d6054e0246c0bc9b4fe4dce697915490b1ce9d5a
Xenon5443 <mtmisola@up.edu.ph>
2026-07-09 08:10:17 +0800

    docs: Update SQL migration scripts

--- scripts/08_assessment.sql
+++ scripts/08_assessment.sql
@@ -3,9 +3,11 @@ create table public.assessment (
   id uuid not null default gen_random_uuid (),
   program_id uuid not null,
   type public.assessment_type not null,
-  questions text[] null,
+  questions jsonb null,
   created_at timestamp with time zone null default now(),
   updated_at timestamp with time zone null default now(),
   constraint assessment_pkey primary key (id),
   constraint assessment_program_id_fkey foreign KEY (program_id) references training_program (id)
 ) TABLESPACE pg_default;
+
+create index IF not exists idx_assessment_program_id on public.assessment using btree (program_id) TABLESPACE pg_default;
